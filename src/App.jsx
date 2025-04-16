77f04f7bd0cd1c9aba72d54ffc8eb33a857460d2
Shafiq-devo <shafique.shakir9877@gmail.com>
2025-04-16 16:14:42 +0500

    xx

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function App() {
         </p>
 
         <p>
-          <label htmlFor="rotate"> second circle</label>
+          <label htmlFor="rotate"> second wer circle</label>
           <input
             type="number"
             id="rotate"
